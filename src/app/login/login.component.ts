bd3c21f152563f095e7df00e08e662598540d6b3
nalon03-github <nalongrace03@gmail.com>
2024-05-02 17:21:22 +0000

    Form Validation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,31 @@
 import { Component, ElementRef } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { CommonModule, NgClass } from '@angular/common';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+
 
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, NgClass],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, NgClass, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  constructor( private fb: FormBuilder){}
+
+  loginForm = this.fb.group({
+    email:['',[Validators.required, Validators.email]],
+    password:['',[Validators.required, Validators.minLength(6)]]
+  });
+
+  // get email(){
+  //   return this.loginForm.get('email');
+  // }
+
+  // get password(){
+  //   return this.loginForm.get('password');
+  // }
 }
