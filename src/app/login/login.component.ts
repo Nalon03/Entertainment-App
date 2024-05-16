809b29448a7e07af311dbc9c1d64282eec1058f8
nalon03-github <nalongrace03@gmail.com>
2024-05-16 10:39:09 +0000

    Added a minimum lenght to the password validation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,10 @@
 import { Component, ElementRef, inject } from '@angular/core';
-import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import {
+  Router,
+  RouterLink,
+  RouterLinkActive,
+  RouterOutlet,
+} from '@angular/router';
 import { CommonModule, NgClass } from '@angular/common';
 import {
   FormBuilder,
@@ -27,9 +32,11 @@ export class LoginComponent {
   loginForm: FormGroup;
   url = 'https://entertainment-api-e1wq.onrender.com';
 
-
-  constructor(private fb: FormBuilder, private router: Router, private http: HttpClient) {
-    
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private http: HttpClient
+  ) {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required],
@@ -41,18 +48,20 @@ export class LoginComponent {
       const formData = this.loginForm.value;
       const localUsers = localStorage.getItem('ent-users');
 
-      this.http.post(this.url + "/login ", {
-        "email": formData.email,
-        "password": formData.password
-      }).subscribe({
-        next: (res) => {
-          console.log(res);
-        },
-        error: (err) => {
-          console.log(err);
-        }
-      })
-  
+      this.http
+        .post(this.url + '/login', {
+          email: formData.email,
+          password: formData.password,
+        })
+        .subscribe({
+          next: (res) => {
+            console.log(res);
+          },
+          error: (err) => {
+            console.log(err);
+          },
+        });
+
       if (localUsers != null) {
         const users = JSON.parse(localUsers);
         users.push(formData);
@@ -61,13 +70,11 @@ export class LoginComponent {
         const users = [formData];
         localStorage.setItem('ent-users', JSON.stringify(users));
       }
-     
-      localStorage.setItem('token', 'Okayyy Yes')
+
+      localStorage.setItem('token', 'Okayyy Yes');
       this.router.navigate(['/home']);
-      
     }
   }
-  
 
   loginObj: Login = new Login();
 }
